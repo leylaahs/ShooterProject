bcd08068b84598c72e622369de1e4be1d6df6696
RasulAliyarov <rasulaliyarov2002@gmail.com>
2022-12-13 01:46:53 -0800

    weapon

---
+++ ConsoleApp3/Models/Weapon.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Models
+{
+    internal class Weapon
+    {
+        int BulletCapacity;
+        int BulletCount;
+        int DischargeSeconds;
+        bool ShootingMode;
+       
+
+
+        public void Shoot()
+        {
+            if (ShootingMode==true)
+            {
+                if (BulletCapacity > 0)
+                {
+                    Console.WriteLine("1 gülle atır");
+                }
+            }
+            else
+            {
+                BulletCapacity = BulletCapacity - 1;
+                Console.WriteLine(" ");
+            }
+        }
+        public void Fire()
+        {
+             int str=(BulletCapacity/DischargeSeconds)*BulletCount;
+            Console.WriteLine("hamısı atəşlənir ");
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine(BulletCapacity-BulletCount);
+        }
+       public void Reload()
+        {
+            if (BulletCount== BulletCapacity)
+            {
+                Console.WriteLine(" darağı yenidən doldurur.");
+            }
+        }
+        public void ChangeFireMode()
+        {
+            if (ShootingMode == false)
+            {
+                Console.WriteLine("Atış modunu dəyişir.");
+            }
+        }
+        public Weapon(int bulletCapacity,int bulletCount,int dischargeSeconds,bool shootingMode)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletCount = bulletCount;
+            DischargeSeconds=dischargeSeconds;
+            ShootingMode = shootingMode;
+
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp3.Models;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(20, 47, 67, true);
+            CreateMenu();
+
+            Console.Write("Reqem secim: ");
+            int acarsoz = int.Parse(Console.ReadLine().Trim());
+
+            switch (acarsoz)
+            {
+                case 1:
+                    weapon.Shoot();
+                    break;
+                case 2:
+                    weapon.Fire();
+                    break;
+                case 3:
+                    weapon.GetRemainBulletCount();
+                    break;
+                case 4:
+                    weapon.Reload();
+                    break;
+                case 5:
+                    weapon.ChangeFireMode();
+                    break;
+            }
+
+        }
+        private static bool CreateMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("0) İnformasiya almaq üçün");
+            Console.WriteLine("1) Shoot metodu üçün");
+            Console.WriteLine("2) Fire metodu üçün");
+            Console.WriteLine("3) GetRemainBulletCount metodu üçün");
+            Console.WriteLine("4) - Reload metodu üçün");
+            Console.WriteLine("5)ChangeFireMode metodu üçün");
+            Console.WriteLine("6)  Proqramdan dayandırmaq üçün\r\nqısayoldur.");
+
+            return true;
+
+        }
+    }
+}
